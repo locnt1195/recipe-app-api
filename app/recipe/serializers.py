2bf23bf0c9693d450ea3318132c2d07875ba82d7
locnt <locnt1195@gmail.com>
2020-07-30 11:04:09 +0700

    Refactor source code

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,6 +1,3 @@
-from django.contrib.auth import get_user_model, authenticate
-from django.utils.translation import ugettext_lazy as _
-
 from rest_framework import serializers
 from core.models import Tag
 
